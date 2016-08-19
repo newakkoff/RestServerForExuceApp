419e5a735ecd555981d787f0456f4e32786c4d3c
newakkoff <newakkoff@gmail.com>
2016-08-19 15:07:49 +0400

    Репозиторий создан

---
+++ src/test/java/com/newakkoff/RestServerForExuceAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.newakkoff;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestServerForExuceAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
